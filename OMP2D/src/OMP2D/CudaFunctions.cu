4009c9820a66dd4a869901fbe581cd097439c63c
Matt Simons <simonsm1@aston.ac.uk>
2014-04-07 11:22:21 +0100

    added power function

--- OMP2D/src/OMP2D/CudaFunctions.cu
+++ OMP2D/src/OMP2D/CudaFunctions.cu
@@ -1,3 +1,24 @@
+#define colIndex(i,j,ld) (((j)*(ld))+(i))
+
+/**
+ *  implementation of the Hillis & Steel inclusive scan reduction method
+ */
+__device__ void reduce(double *v, int length)
+{
+	extern __shared__ double sdata[];
+
+	int index = blockIdx.x*threadIdx.x + threadIdx.x;
+	int index2 = index * 2 + 1;
+
+	if(index2 < length)
+	{
+		sdata[index] = v[index2] + v[index2 - 1];
+	}
+	__syncthreads();
+
+	v[index] = sdata[index];
+}
+
 extern "C"
 __global__ void scale(double *v1, int length, double factor)
 {
@@ -9,16 +30,51 @@ __global__ void scale(double *v1, int length, double factor)
 }
 
 extern "C"
-__global__ void innerProduct(double *v1, double *v2, int length)
+__global__ void power(double *v1, double factor)
 {
 	int index = blockIdx.x*threadIdx.x + threadIdx.x;
 	if(index < length)
 	{
-		v1[index] = v1[index] * v2[index];
+		v1[index] = pow(v1[index], factor);
+	}
+}
+
+extern "C"
+__global__ void innerProduct(double *v1, double *v2, int length, double *ans)
+{
+	int index = blockIdx.x*threadIdx.x + threadIdx.x;
+	extern __shared__ double products[];
+
+	// Find the product of each value
+	if(index < length)
+	{
+		products[index] = v1[index] * v2[index];
+		//printf("product of %f * %f = %f\n", v1[index], v2[index], products[index]);
 	}
 	__syncthreads();
 
-	reduce(v1, length);
+	// Sum all the products
+	while(length != 0)
+	{
+		reduce(products, length);
+		length = length / 2;
+		__syncthreads();
+	}
+
+	// First thread copies answer to global mem
+	if(index == 0) {
+		ans[0] = products[0];
+	}
+}
+
+extern "C"
+__global__ void multiply(double *m1, double *m2, int width, int height, double *result)
+{
+	int index = blockIdx.x*threadIdx.x + threadIdx.x;
+	int corresponding =
+
+	//multiply each row and column and reduce atomic add maybe
+
 }
 
 
@@ -32,76 +88,4 @@ __global__ void vectorDot(double *v1, double *v2, int length, double *v3)
 		v3[index] = v1[index] * v2[index];
 	}
 	// returns *v3
-}
-
-__device__ void reduce(double *v1, int length)
-{
-	__shared__ double sdata[1024];
-	int index = blockIdx.x*threadIdx.x + threadIdx.x;
-	int index2 = index * 2 + 1;
-	//int index2 = length/2+index;
-	if(index2 < length)
-	{
-		sdata[index] = v1[index2] + v1[index2 - 1];
-		//v1[index] = v1[index] + v1[index2];
-	}
-	__syncthreads();
-	v1[index] = sdata[index];
-	//printf("%f ", v1[index]);
-}
-
-extern "C"
-__global__ void shmem_reduce_kernel(double *d_out, double *d_in)
-{
-
-    // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
-    extern __shared__ double sdata[];
-
-    int myId = threadIdx.x + blockDim.x * blockIdx.x;
-    int tid  = threadIdx.x;
-
-    // load shared mem from global mem
-    sdata[tid] = d_in[myId];
-
-    __syncthreads();            // make sure entire block is loaded!
-
-    // do reduction in shared mem
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-    {
-        if (tid < s)
-        {
-            sdata[tid] += sdata[tid + s];
-
-        }
-        __syncthreads();        // make sure all adds at one stage are done!
-    }
-
-    // only thread 0 writes result for this block back to global mem
-    if (tid == 0)
-    {
-        d_out[blockIdx.x] = sdata[0];
-    }
-}
-
-extern "C"
-__global__ void global_reduce_kernel(float * d_out, float * d_in)
-{
-    int myId = threadIdx.x + blockDim.x * blockIdx.x;
-    int tid  = threadIdx.x;
-
-    // do reduction in global mem
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-    {
-        if (tid < s)
-        {
-            d_in[myId] += d_in[myId + s];
-        }
-        __syncthreads();        // make sure all adds at one stage are done!
-    }
-
-    // only thread 0 writes result for this block back to global mem
-    if (tid == 0)
-    {
-        d_out[blockIdx.x] = d_in[myId];
-    }
 }
